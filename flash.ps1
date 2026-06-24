cc69353db07b71bf4db8223ad026cf86123253d5
misikovich <misikovich.dev@gmail.com>
2026-06-24 19:11:59 +0200

    removed os specific configs from repo

--- flash.ps1
+++
@@ -1,8 +0,0 @@
-# Flash out/pwr-probe-test/default.hex to PIC24EP256MC206 via PICkit 4
-# Usage: .\flash.ps1
-# Requires: PICkit 4 connected, target powered or use -W flag for PICkit-supplied power
-
-$ipecmd  = "C:\Program Files\Microchip\MPLABX\v6.20\mplab_platform\mplab_ipe\ipecmd.exe"
-$hex     = "$PSScriptRoot\out\pwr-probe-test\default.hex"
-
-& $ipecmd -TPPK4 -P24EP256MC206 -F"$hex" -M -OL
